d7600e1fbeb3de1846358371bfda0f341730b8f4
MAMON-QA <mamon.green.edu.bd@gmail.com>
2024-05-30 19:52:49 +0600

    last

--- routes/products.php
+++ routes/products.php
@@ -4,21 +4,16 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\Product\ProductController;
 
 
-
-
-
-
 //product All route
 Route::prefix('product')->group(function(){
-     Route::get('/manage', [ProductController::class, 'ProductView'])->name('product.manage');
-     Route::get('/add', [ProductController::class, 'ProductAdd'])->name('product.add');
-     Route::post('/store', [ProductController::class, 'ProductStore'])->name('product.store');
-      Route::get('/edit/{id}', [ProductController::class, 'ProductEdit'])->name('product.edit');
-      Route::post('/update/product', [ProductController::class, 'ProductUpdate'])->name('product.update');
-      Route::post('/update/multi-image', [ProductController::class, 'ProductUpdateMultiImage'])->name('update.product.multi_image');
-      Route::get('/delete/{id}', [ProductController::class, 'ProductDelete'])->name('product.delete');
-});
-
+      Route::get('/manage', [ProductController::class, 'ProductView'])->name('product.manage');
+      Route::get('/add', [ProductController::class, 'ProductAdd'])->name('product.add');
+      Route::post('/store', [ProductController::class, 'ProductStore'])->name('product.store');
+       Route::get('/edit/{id}', [ProductController::class, 'ProductEdit'])->name('product.edit');
+       Route::post('/update/product/{id}', [ProductController::class, 'ProductUpdate'])->name('product.update');
+       Route::post('/update/multi-image', [ProductController::class, 'ProductUpdateMultiImage'])->name('update.product.multi_image');
+       Route::get('/delete/{id}', [ProductController::class, 'ProductDelete'])->name('product.delete');
+ });
 
 
 
